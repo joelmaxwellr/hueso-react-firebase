42e940115cd0f8ef23bb068cfd48b82de4f53a1d
Joel Maxwell Reinoso Taveras <joelmaxwellr@gmail.com>
2023-07-27 01:55:01 -0400

    api

--- src/PrintButton.jsx
+++ src/PrintButton.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from "react";
+
+
 
 
 const PrintButton = ({ objeto, mostrarBoton }) => {
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import firebaseConfig from "./firebase-config.js"
+import firebaseConfig from "./firebase-config.jsx"
 import {FirebaseAppProvider} from "reactfire"
-
+import { useState, useEffect } from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
